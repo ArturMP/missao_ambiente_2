a81314dd46a478f958362cf4ddf25d859e7f8bca
ArturMP <114751420+ArturMP@users.noreply.github.com>
2024-08-30 15:56:17 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,109 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola, você se depara com um grupo de jovens maltratando um animal silvestre, qual é a sua reação?",
+        alternativas: [
+            {
+                texto: "Eu ligaria para a policia ambiental e esperaria ela chegar para puni-los",
+                afirmacao: [
+                    "No início pensou em ignorar a situação, para evitar problemas, porém decidiu fazer a coisa certa.",
+                    "Apesar de ser tentador não se envolver em problemas, você decidiu fazer as escolhas certas"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Eu não me envolveria e passaria calado",
+                afirmacao: [
+                    "Por mais que a situação fosse ruim, fazer o bem seria muito arriscado para mim",
+                    "Devido aos riscos de se envolver em confusões, vocẽ recuou na hora de lutar pelo planeta"
+                ],
+                proxima: 1,
+            },
+        ]
+    },      
+    {
+        enunciado: "Enquanto assistia televisão, você deparou com inúmeras notícias sobre a atual situação da saúde ecológica do país, que por sinal, não parece estar muito boa, oque você pensa?",
+        alternativas: [
+            {
+                texto: "Eu deveria fazer a minha parte para tentar mudar essa situação e icentivar os outros a fazer o mesmo.",
+                afirmacao: [
+                    "Diminuir a poluição é díficil,  porém você sabe que com pequenos esforços pode melhorar o meio ambiente.",
+                    "Ao entender que é preciso alguns esforços para salvar o planeta, você se sente movitvado com notícias do assunto",
+                ],
+                proxima: 2,
+            },
+            {
+                texto: "Não há o que eu possa fazer, independente da boa ação, o mundo vai acabar um dia mesmo.",
+                afirmacao: [
+                    "No fundo, ocê ficou desacreditado de que o mundo possa melhorar",
+                    "Parecia que o mundo estava entrando em colapso, então não acreditava em salvação",
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Durante a aula, a professora apresenta um projeto voluntário sobre a conscientização o descarte correto do lixo e reciclagem, e convida os alunos a participarem, o que você decide fazer",
+        alternativas: [
+            {
+                texto: "É uma boa ideia, mesmo não valendo nota, eu deveria participar para tornar o mundo um lugar melhor",
+                afirmacao: [
+                    "Separar o lixo é trabalhoso, entretando você sabe que é o melhor para o meio ambiente",
+                    "As vezes, fazer algo para o planeta pode não parecer ter efeito, mas nem por isso desistiu.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Eu não vou particiar, preciso fazer outras coisas além disso",
+                afirmacao: [
+                    "As outras preocupações da vida te impediram de enxergar o futuro do planeta",
+                    "Sentiu que precisava resolver outras coisas mais importantes, do que conservar o planeta.",
+                ],
+                proxima: 3,
+            },
+        ]
+    },
+    {
+        enunciado: "Na escola, você esta andando com um amigo e ele joga um papel de bala no chão, você faz alguma coisa?",
+        alternativas: [
+            {
+                texto: "Eu corrigo ele e peço para ele pegar o papel do chão para descartar no ligar correto",
+                afirmacao: [
+                    "Para você, o correto é 'lixo no lixo', se não tem um lixo proximo, segure-o até encontrar um local de descarte correto.",
+                    "Nos seus olhos, tem horas que o correto precisa ser feito, mesmo gerando alguma situação desagradável."
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Eu não faço nada, um papel de bala não vai fazer diferença",
+                afirmacao: [
+                    "Para você, um pequeno papel não faz diferença, mas para o planeta faz.",
+                    "Aos seus olhos, algumas coisas pequenas não influenciam na saúde do meio ambiente."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    
+    {
+        enunciado: "A temperatura está razoavel,e o ar-condicionado esta ligado, você olha e pensa",
+        alternativas: [
+            {
+                texto: "Deveria desliga-lo para economizar um pouco de energia e aproveitar o clima natural",
+                afirmacao: [
+                    "Pensando no própio consumo, você salvou um pouco do planeta para que as próximas gerações possam viver.",
+                    "Sendo um consumidor consciente, você tentou resguardar um pouco o meio ambiente."
+                ],
+              
+            },
+            {
+                texto: "Vou deixar ligado, se eu tenho vou usar mesmo",
+                afirmacao: [
+                    "Você pensa em si mesmo e não no impacto do meio ambiente, sendo assim, não aproveita o que a natureza pode te oferecer e impede as futuras gerações também.",
+                    "Colocando você em primeiro plano, se esquecendo do o meio ambiente e colocando-os em xeque."
+                ],
+                
+            },
+        ]
+    },
+
+];
