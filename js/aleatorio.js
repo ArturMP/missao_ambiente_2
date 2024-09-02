2361d91040eccb01841b390e2bff888ee9eaf208
ArturMP <114751420+ArturMP@users.noreply.github.com>
2024-09-02 08:56:14 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["Artur", "Luana", "Lucas", "Kailany", "Amanda", "Gustavo", "Gabriel","Nicolas","André","Leonardo"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
